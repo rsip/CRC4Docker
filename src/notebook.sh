c836e8afbf1136a3293c05d76ab1ec62ab688b9b
mortcanty <mort.canty@gmail.com>
2018-10-23 20:06:27 +0200

    took out the password from notebook.sh again

--- src/notebook.sh
+++ src/notebook.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jupyter notebook --no-browser --port 8888 --ip=* --allow-root --NotebookApp.token='' --NotebookApp.password='sha1:542e12d99ca8:d1f3b72830eacc50ad7102ddc2f1064934473428'
+jupyter notebook --no-browser --port 8888 --ip=* --allow-root --NotebookApp.token=''
